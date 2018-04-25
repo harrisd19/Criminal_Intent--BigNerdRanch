b4217df91c7eaa8a5a921a9cd719e570e029d84b
Devon <stdsh07@moravian.edu>
2018-04-24 23:36:09 -0400

    New Stuff

---
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeListActivity.java
@@ -0,0 +1,12 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeListFragment();
+    }
+}
